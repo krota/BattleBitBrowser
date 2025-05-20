5b35cc06f0082d026521a60d6658e80a69df96a3
Kyle Rota <Kyle.Rota@gmail.com>
2025-05-19 19:43:56 -0700

    Corrected property name

--- Models/SteamNewsItemViewModel.cs
+++ Models/SteamNewsItemViewModel.cs
@@ -5,6 +5,6 @@ public class SteamNewsItemViewModel
     public string? Title { get; set; }
     public string? Url { get; set; }
     public string? Author { get; set; }
-    public string? SanitizedContents { get; set; }
+    public string? ContentsHtml { get; set; }
     public DateTime Date { get; set; }
 }
